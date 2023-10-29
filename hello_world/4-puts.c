73036b0c8f5c217bf95798206c94d7e951e03fb7
vnporras <vnporras1028@gmail.com>
2023-10-29 15:44:16 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - This is the main function for 4-puts.c.
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

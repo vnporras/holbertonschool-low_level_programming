8b0e0e3abe5c1910eb9161f56dc2ae00decd0924
vnporras <vnporras1028@gmail.com>
2023-10-31 08:16:32 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 if (last_digit == 0)
 {
-printf("Last digit of %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 if (last_digit < 6 && last_digit != 0)
 {

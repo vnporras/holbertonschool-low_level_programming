9f9339ff35d491ee0ff5cb4732efbe4f14d0108f
vnporras <vnporras1028@gmail.com>
2023-10-31 11:39:34 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is the main function for 5-print_numbers.c.
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+char number;
+for (number = 0; number < 10 ; number++)
+{
+putchar(number);
+}
+return (0);
+}

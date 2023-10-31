b719b8c582b49800636e6e1a4763f605f76484f4
vnporras <vnporras1028@gmail.com>
2023-10-31 12:05:07 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is the main function for 6-print_numberz.c.
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10 ; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}

b48583b449d613848c3cfeaeb12a6324e1de0211
vnporras <vnporras1028@gmail.com>
2023-11-01 10:44:05 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - This is the main function for 1-alphabet.c.
+ * Return: Description of the returned value
+ */
+void print_alphabet(void)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+return (0);
+}

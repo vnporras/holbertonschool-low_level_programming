f9722364717b2a264f5a7866a74371e92e6e10a9
vnporras <vnporras1028@gmail.com>
2023-11-01 08:43:40 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function for 0-putchar.c.
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+char put[20] = "_putchar";
+puts(put);
+return (0);
+}

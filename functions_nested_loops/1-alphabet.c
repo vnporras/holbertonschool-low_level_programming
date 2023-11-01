ecc55e04d3c12af7a3255560ae92303039640f59
vnporras <vnporras1028@gmail.com>
2023-11-01 12:42:27 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
  */
 void print_alphabet(void)
 {
-char l;
-for (l = 'a'; l <= 'z'; l++)
-{
-_putchar(l);
-}
-_putchar('\n');
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
 }

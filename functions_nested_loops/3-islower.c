c128264889c04e9e68e5a400a6fb0efe6e39c948
vnporras <vnporras1028@gmail.com>
2023-11-02 10:58:32 -0700

    add 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+include <stdio.h>
 #include <ctype.h>
 #include "main.h"
 
 /**
  * _islower - This is the function.
- * c - Integer that represents a character.
+ * @c: Integer that represents a character.
  * Return: Description of the returned value
  */
 int _islower(int c)
 {
-	if (c >= 97 && c<=122)
+	if (c >= 97 && c<= 122)
 
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}

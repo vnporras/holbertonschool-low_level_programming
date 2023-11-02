9df272755b80c9f8386997150bc2433e7a13c727
vnporras <vnporras1028@gmail.com>
2023-11-02 12:26:40 -0700

    add file 5-sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
  */
 int print_sign(int n)
 {
-	if (n > 0 )
-        {
+	if (n > 0)
+	{
 		_putchar('+');
-                return (1);
+		return (1);
         }
 	else if (n == 0)
         {
 		_putchar('0');
-                return (0);
+		return (0);
         }
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);

33c0179b6d2c246a01b74be8919a9dc05ad2b812
vnporras <vnporras1028@gmail.com>
2023-11-02 13:13:16 -0700

    add file 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - This is the function.
+ * @int type
+ * Return: Description of the returned value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}

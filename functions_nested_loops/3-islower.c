344397565877a0be0b3eaecc0a5244064d1a54b6
vnporras <vnporras1028@gmail.com>
2023-11-01 20:28:02 -0700

    Add 3-islower function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - This is the function.
+ * c - Integer that represents a character.
+ * Return: Description of the returned value
+ */
+int _islower(int c)
+{
+	char alpha = _putchar(c);
+
+	if (islower(alpha) == 1)
+	{
+		return (1);
+	}
+	return (0);
+}

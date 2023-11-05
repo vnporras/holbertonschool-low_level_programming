ecc7cd638eeafbb0b6aef4af1a6f771a4143db2e
vnporras <vnporras1028@gmail.com>
2023-11-05 14:25:18 -0800

    add file 7-print_last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,13 @@
 
 int print_last_digit(int n)
 {
-	int ld;
-	
+	int ld;	
 	ld = n % 10;
 	
-	if (ld < 0)
+	if(ld < 0)
 	{
 		ld = ld * -1;
 	}
 	_putchar(ld + '0');
-	return(ld);
+	return (ld);
 }

79275eb6d2d823836e144da615c45965db94b94f
vnporras <vnporras1028@gmail.com>
2023-11-06 04:32:36 -0800

    add file 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * add - This is a funcion.
+  * @num1: Integer that represents a character
+  * @num2: Integer that represents a character
+  *
+  * Return: Description of the returned value
+  */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+

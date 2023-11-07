24fd3fd30e80516e28a49cb656246cc6f0f7848f
vnporras <vnporras1028@gmail.com>
2023-11-07 10:50:30 -0800

    add file 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - this is a function
+ * @n: Integer that represents a character
+ */
+void swap_int(int *a, int *b)
+{
+	int v;
+	v = *a;
+	*a = *b;
+	*b = v;
+}

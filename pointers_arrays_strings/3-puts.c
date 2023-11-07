0efb4d4804478e7fedd6c7cefe48dfe37c4a08fc
vnporras <vnporras1028@gmail.com>
2023-11-07 14:54:54 -0800

    Add file 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _puts - this is a function
+ * @str: String to be printed.
  * Return: Description of the returned value
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0') 
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;

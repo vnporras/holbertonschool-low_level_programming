3a88f8844b939fc6a7b3def834eacd9c526838f6
vnporras <vnporras1028@gmail.com>
2023-11-07 08:34:53 -0800

    add file 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,6 @@
+#include "main.h"
+void reset_to_98(int *n)
+{
+	int v= 98;
+	*n = v;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+
+
+
+
+
+
+
+
+
+#endif

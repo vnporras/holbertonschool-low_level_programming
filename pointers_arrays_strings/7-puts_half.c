5e7e4905285914b7b64ceadf544d696e1964743e
vnporras <vnporras1028@gmail.com>
2023-11-08 20:02:40 -0800

    add file 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - this is a function
+ * @str: Declaration.
+ * Return: Description of the returned value
+ */
+
+void puts_half(char *str)
+{
+	int i, v, size;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+	if (size % 2 == 0)
+	{
+		for (v = size / 2; v < size; v++)
+		{
+			_putchar(str[v]);
+		}
+	}
+	else
+	{
+		for (v = (size - 1) / 2; v < size; v++)
+		{
+			_putchar(str[v]);
+		}
+	}
+	_putchar('\n');
+}

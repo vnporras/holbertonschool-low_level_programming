9c61eadc95d64bfba5bed08b0e8fa3c81fec784e
vnporras <vnporras1028@gmail.com>
2023-11-11 19:46:06 -0800

    add file 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - this is a function
+ * @s: Pointer.
+ * Return: Description of the returned value.
+ */
+
+int _atoi(char *s)
+{
+	int i, size, sign, result;
+
+	sign = 1;
+	result = 0;
+
+	for (size = 0; s[size] != '\0'; size++)
+		;
+
+	for (i = 0; i < size; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+	}
+	return (result * sign);
+}

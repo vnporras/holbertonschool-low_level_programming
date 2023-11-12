c4a1cc0b5263d27c65cc1e7b90749aa419e49dc0
vnporras <vnporras1028@gmail.com>
2023-11-11 17:33:29 -0800

    add file 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - this is a function
+ * @a: Declaration.
+ * @n: Declaration.
+ * Return: Description of the returned value
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}

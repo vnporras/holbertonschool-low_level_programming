7c3c486bf03c257084664ba392056532275a2314
vnporras <vnporras1028@gmail.com>
2023-11-11 18:47:13 -0800

    add file 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - this is a function
+ * @dest: Pointer destination.
+ * @src: Source pointer.
+ * Return: Description of the returned value.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int size, i;
+
+	for (size = 0; src[size] != '\0'; size++);
+
+	for (i = 0; i< size; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[size] = '\0';
+
+	return (dest);
+}

ffe90f3300d7c3b916793ede50cb9ea46a23fe74
vnporras <vnporras1028@gmail.com>
2023-11-13 10:34:52 -0800

    add file 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - check the code
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}

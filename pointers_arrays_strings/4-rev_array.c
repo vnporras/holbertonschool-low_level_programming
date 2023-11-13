8e11017d41147871aa9a603b40d9a0d862397e10
vnporras <vnporras1028@gmail.com>
2023-11-13 06:37:02 -0800

    add file 4-rev

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i = 0;
-	int j = n - 1;
-	int temp;
-	while (i < j)
+	int i, j, temp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
-		i++;
-		j--;
 	}
 }

c44bbc4f2b3f3e270a1193b97cd53509b8e99004
vnporras <vnporras1028@gmail.com>
2023-11-12 16:48:27 -0800

    add file 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - Function
+ * @dest: Pointer destination.
+ * @src: Source pointer.
+ * @n: Character.
+ * Return: Description of the returned value.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int size;
+
+	for (size = 0; src[size] != '\0' && size < n; size++)
+	{
+		dest[size] = src[size];
+	}
+	dest[size] = '\0';
+	return (dest);
+}

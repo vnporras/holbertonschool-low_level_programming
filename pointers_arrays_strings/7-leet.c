41025b83d87aea9821f66ffba3e3056bb83bb7e0
vnporras <vnporras1028@gmail.com>
2023-11-13 06:19:05 -0800

    add file 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,8 +4,6 @@
 /**
  * *leet - Function
  * @n: Source pointer.
- * @9i: Variable.
- * @j: Variable..
  * Return: Description of the returned value.
  */
 
@@ -14,7 +12,7 @@ char *leet(char *n)
 	int i, j;
 	char *s = "aAeEoOtTlL";
 	char *s1 = "4433007711";
-	
+
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)

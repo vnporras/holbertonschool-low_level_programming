0cddc184cd61d0b18fee0714343d0a01c92ef4cc
vnporras <vnporras1028@gmail.com>
2023-11-13 17:46:17 -0800

    add file 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 #include "main.h"
 /**
  * _puts_recursion - check the code
- *
+ *@s: Is a Pointer.
  * Return: Always 0.
  */
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	for (; s[i] != '\0'; i++)
+	if (*s == '\0')
 	{
-		putchar(s[i]);
+		_putchar('\n');
+		return (0);
 	}
-	putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }

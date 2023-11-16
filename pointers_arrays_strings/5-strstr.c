6729f069f4ba0acd55b56f054585d8308d2ff7d5
vnporras <vnporras1028@gmail.com>
2023-11-15 20:51:54 -0800

    add file 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - this is a function
+ * @haystack: Pointer Declaration.
+ * @needle: Pointer Declaration.
+ * Return: Description of the returned value
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+	
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = i, j = 0; needle[j] != '\0'; j++, k++)
+		{
+			if (needle[j] != haystack[k] || haystack[k] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}

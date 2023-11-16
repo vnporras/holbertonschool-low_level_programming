efe3f294ba0eb103e5d81afe4b9746e370403284
vnporras <vnporras1028@gmail.com>
2023-11-15 18:26:57 -0800

    add the 0-argument

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - This is a function.
-*/
+/**
+ * main - This is an argc and argv function.
+ * @argc: Is a count argument.
+ * @argv: Is a argument.
+ * Return: Description of the returned value
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;

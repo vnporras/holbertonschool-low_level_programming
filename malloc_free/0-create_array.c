f0e25b9179ae2e95992a19c7ae2a71bbd196f83a
vnporras <vnporras1028@gmail.com>
2023-11-17 12:29:34 -0800

    add file 0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *create_array - This is an argc and argv function.
+ * @size: size of type.
+ * @c: Is a type.
+ * Return: Description of the returned value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+	a = malloc(sizeof(char) * size);
+
+	if (a == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}

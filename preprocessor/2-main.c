ff777837a8e6c69bdc5593ec64b507e057443094
vnporras <vnporras1028@gmail.com>
2023-11-21 15:58:59 -0800

    add file 2-main.c

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - This is an argc and argv function.
+ * @void: Is a parameter.
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);

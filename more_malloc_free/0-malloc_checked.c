01f560dbf564f84272218b8d0e8c20bf9390040b
vnporras <vnporras1028@gmail.com>
2023-11-21 08:42:49 -0800

    add file 0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - This is a function.
+ * @b: Is argument.
+ * Return: Description of the returned value
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *newPointer;
+	newPointer = malloc(b);
+
+	if (newPointer == NULL)
+	{
+		exit(98);
+	}
+	return (newPointer);
+}

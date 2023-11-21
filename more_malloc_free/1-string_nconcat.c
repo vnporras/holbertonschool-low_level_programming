65646dcb64b55f94abf83e05bf85a3822d1730cb
vnporras <vnporras1028@gmail.com>
2023-11-21 12:17:04 -0800

    add file 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = s2Size;
 	}
-	sum = malloc(sizeof(char) * (s1Size + n + 1));
+	sum = malloc(sizeof(char) * (s1Size + s2Size + 1));
 	if (sum == NULL)
 	{
 		return (NULL);

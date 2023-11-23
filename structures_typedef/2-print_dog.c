9cc1007d9a4f243f88440797629e9371b0569b63
vnporras <vnporras1028@gmail.com>
2023-11-23 09:47:02 -0800

    add file 2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * print_dog - Is a function.
+  * @d: Is a pointer.
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		if (d->name == NULL || d->owner == NULL)
+		{
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("%s\n", d->name);
+			printf("%s\n", d->owner);
+		}
+		printf("%f\n", d->age);
+	}
+	else
+	{
+		printf("Name:%s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}

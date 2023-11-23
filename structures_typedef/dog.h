678bce597de48782789872fb8ca8f58f27425957
vnporras <vnporras1028@gmail.com>
2023-11-23 07:23:06 -0800

    chage in file dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
-typedef struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Is an alias.
+* @name: Is a name.
+* @age: Is an age.
+* @owner: Is an owner.
+*
+* Description: This is a structor, include name, age and owner..
+*/
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}my_dog;
+};
+#endif

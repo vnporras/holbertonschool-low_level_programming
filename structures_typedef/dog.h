4395147b12a4f0b464df02edf0011ae5535330ac
vnporras <vnporras1028@gmail.com>
2023-11-24 07:44:19 -0800

    add new type dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
 * struct dog - Is an alias.
 * @name: Is a name.
@@ -17,12 +18,11 @@ struct dog
 	char *owner;
 };
 
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
+/**
+ *typedef struct dog dog_t - Change the name dog for dog_t
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

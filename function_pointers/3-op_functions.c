3baacb1ea81140e6a753d6859a7e8919e54d980a
vnporras <vnporras1028@gmail.com>
2023-11-25 19:20:46 -0800

    changes in operations div and mod of file 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -39,6 +39,11 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (a / 2);
+	}
 	return (a / b);
 }
 /**
@@ -49,5 +54,10 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (a / 2);
+	}
 	return (a % b);
 }

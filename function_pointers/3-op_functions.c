28163f482335cffc972c014105ae013b7f123d36
vnporras <vnporras1028@gmail.com>
2023-11-25 19:05:16 -0800

    changes in description of file 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,35 +2,51 @@
 
 /**
  * op_add - Is a function.
- * op_sub - Is a function.
- * op_mul - Is a function.
- * op_div - Is a function.
- * op_mod - Is a function.
  * @a: first integer.
  * @b: second integer.
- * Return: sum of a and b.
+ * Return: Isthe result of operation.
  */
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+*op_sub - Is a function.
+* @a: first integer.
+* @b: second integer.
+* Return: Isthe result of operation.
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+* op_mul - Is a function.
+* @a: first integer.
+* @b: second integer.
+* Return: Isthe result of operation.
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+* op_div - Is a function.
+* @a: first integer.
+* @b: second integer.
+* Return: Isthe result of operation.
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
+/**
+* op_mod - Is a function.
+* @a: first integer.
+* @b: second integer.
+* Return: Isthe result of operation.
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);

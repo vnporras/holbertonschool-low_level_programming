f0e9e988a3ef9beec67271b886920e21af95160f
vnporras <vnporras1028@gmail.com>
2023-11-25 18:52:43 -0800

    add files 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Is a function.
+ * op_sub - Is a function.
+ * op_mul - Is a function.
+ * op_div - Is a function.
+ * op_mod - Is a function.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

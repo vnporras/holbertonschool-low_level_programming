cd11aca1140111856268517ccdd84dfbd27242a1
vnporras <vnporras1028@gmail.com>
2023-12-14 18:14:59 -0800

    correct output for betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* print_list -  name the function of print all elements of a list.
-* @list_t: list.
+* print_list -  name the function of print all elements.
 * @h: Is a poniter of list.
-* return:  the number of nodes.
+* Return:  the number of nodes.
 */
 
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
+
 	while (h != NULL)
 	{
 		printf("[%i] %s\n", h->len, h->str);

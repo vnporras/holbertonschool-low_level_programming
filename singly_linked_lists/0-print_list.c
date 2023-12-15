8a87c8a07bb2ac11091008996eab5f380aa4caef
vnporras <vnporras1028@gmail.com>
2023-12-14 18:27:34 -0800

    modified the code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,9 +14,16 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%i] %s\n", h->len, h->str);
-		h = h->next;
-		n++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		 h = h->next;
+		 n++;
 	}
 	return (n);
 }

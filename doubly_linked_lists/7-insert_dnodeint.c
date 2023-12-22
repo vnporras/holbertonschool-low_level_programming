71baf9ee5e5df4b2cd8dba823b16b3597dcdf243
vnporras <vnporras1028@gmail.com>
2023-12-22 11:32:15 -0800

    create the file 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 		temporal = temporal->next;
-		i++;
+		++i;
 	}
 	newNode->next = temporal->next;
 	newNode->prev = temporal;

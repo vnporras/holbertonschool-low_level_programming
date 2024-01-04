b4551f701ebf9902ee74ee4bb0a137330b0edbf5
vnporras <vnporras1028@gmail.com>
2024-01-04 14:30:03 -0800

    create the file 1-djb2.c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -31,5 +33,5 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
 #endif

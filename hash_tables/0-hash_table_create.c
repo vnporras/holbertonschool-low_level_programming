145a95674d3f5b7aa24d320dff024769ce2ffedd
vnporras <vnporras1028@gmail.com>
2024-01-04 14:22:02 -0800

    modified betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
 	unsigned long int i = 0;
+
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
